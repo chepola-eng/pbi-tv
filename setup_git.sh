4f56779df03459b7096e5eea19d02c6bb406a414
chepola-eng <chepola@gmail.com>
2026-06-26 11:29:53 -0300

    Delete setup_git.sh

--- setup_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Este script é executado pelo Render antes de cada run
-# para configurar o git com autenticação via token
-
-git config --global credential.helper store
-echo "https://oauth2:${GIT_TOKEN}@github.com" > ~/.git-credentials
-git remote set-url origin "${GIT_REPO_URL}"
